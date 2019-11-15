b4c16e4f6bda6b76debec3d3b3795dd0f1170977
chanshunli <chanshunli@gmail.com>
2019-11-15 20:57:12 +0800

    Add lib py

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [cnuernber/libpython-clj "1.6"]
+                 [techascent/tech.datatype "4.50"]]
   :repl-options {:init-ns matplotlib-clj.core})
